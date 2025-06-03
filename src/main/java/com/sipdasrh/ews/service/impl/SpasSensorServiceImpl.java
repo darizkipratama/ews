44a155ec010e93efdf065cdd6f98415fab0cd6fb
darizkipratama <dpratama@gmail.com>
2025-06-03 17:50:52 +0700

    (init) add looping logic to query all sensors from database

--- src/main/java/com/sipdasrh/ews/service/impl/SpasSensorServiceImpl.java
+++ src/main/java/com/sipdasrh/ews/service/impl/SpasSensorServiceImpl.java
@@ -1,9 +1,16 @@
 package com.sipdasrh.ews.service.impl;
 
+import com.sipdasrh.ews.domain.SpasArrInstall;
+import com.sipdasrh.ews.repository.SpasArrInstallRepository;
 import com.sipdasrh.ews.service.SpasSensorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class SpasSensorServiceImpl implements SpasSensorService {
@@ -11,12 +18,26 @@ public class SpasSensorServiceImpl implements SpasSensorService {
     @Autowired
     RestTemplate restTemplate;
 
+    private static final Logger LOG = LoggerFactory.getLogger(SpasSensorServiceImpl.class);
+
+    private final SpasArrInstallRepository spasArrInstallRepository;
+
+    public SpasSensorServiceImpl(SpasArrInstallRepository spasArrInstallRepository) {
+        this.spasArrInstallRepository = spasArrInstallRepository;
+    }
+
     @Override
     public void getDataFromSensors() {
-        String res = restTemplate.getForObject(
-            "https://tatonas.co.id/api/v2/realtime?uc=Se0q6pbR_all_bpdas_v2&pc=039&hw=4126",
-            String.class
-        );
-        System.out.println(res);
+        LOG.info("Get Data From Sensors API : Start at {}", LocalDate.now());
+        List<SpasArrInstall> spasArrLogList = spasArrInstallRepository.findAll();
+        spasArrLogList.forEach(spasArrLog -> {
+            LOG.info("Get Data From Sensors API : Sensor {}", spasArrLog.getNamaInstalasi());
+            String res = restTemplate.getForObject(
+                spasArrLog.getUrlInstalasi(),
+                String.class
+            );
+            System.out.println(res);
+        });
+        LOG.info("Get Data From Sensors API : End at {}", LocalDate.now());
     }
 }
